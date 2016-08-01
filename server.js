5d5b269c4b7eb9e1ad10265ea9fe1d6871c98858
JD Gonzalez <jdg99@live.com>
2016-08-01 22:17:36 +0000

    added timestamp function for first challenge

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var ts = require('./timestamp');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/:timestamp', function (req, res) {
+  res.send(ts.print(req.params.timestamp));
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Timestamp app listening on port 8080!');
 });
---
+++ timestamp.js
@@ -0,0 +1,21 @@
+var strftime = require('strftime');
+var unixRegex = /^\d+$/;
+var naturalRegex = /(January|February|March|April|May|June|July|August|September|October|November|December)[ ](0[1-9]|[12][0-9]|3[01])[,][ ]((19|20)[0-9][0-9])/;
+
+module.exports = {
+  print: function (timestamp) {
+    console.log(timestamp);
+    var result = {};
+    if (unixRegex.test(timestamp) && timestamp  <= Math.floor(new Date() / 1000)) {
+       result.unix = timestamp;
+       result.natural = strftime('%B %d, %Y', new Date (timestamp*1000));
+    } else if (naturalRegex.test(timestamp)){
+       result.unix = Date.parse(timestamp)/1000;
+       result.natural = strftime('%B %d, %Y', new Date (timestamp));
+    } else {
+      result.unix = null;
+       result.natural = null;
+    }
+    return JSON.stringify(result);
+  }
+};
