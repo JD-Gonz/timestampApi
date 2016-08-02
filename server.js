4d0d60872c37a3ecd004eb7e9e2a42a8ba2c7a73
JD Gonzalez <jdg99@live.com>
2016-08-02 18:38:06 +0000

    more troubleshooting

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var ts = require('./timestamp');
 var path = require('path');
-var app = express();
+var app = express.createServer();
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -11,6 +11,6 @@ app.get('/:timestamp', function (req, res) {
   res.send(ts.print(req.params.timestamp));
 });
 
-app.listen(8080, function () {
-  console.log('Timestamp app listening on port 8080!');
+app.listen(process.env.PORT || 8080, function () {
+  console.log('Timestamp app is listening');
 });
